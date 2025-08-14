bc6dc91af55edbb2ba060140c16102b33bcc6cf2
GiselDiaz <gis_ale13@hotmail.com>
2025-08-14 12:34:53 -0600

    modelos

--- backend/dist/models/respuesta.js
+++ backend/dist/models/respuesta.js
@@ -1,1 +1,41 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const sequelize_1 = require("sequelize");
+const connection_1 = __importDefault(require("../database/connection"));
+class respuestas extends sequelize_1.Model {
+}
+respuestas.init({
+    id: {
+        type: sequelize_1.DataTypes.UUID,
+        allowNull: false,
+        primaryKey: true
+    },
+    id_sesion: {
+        type: sequelize_1.DataTypes.UUID,
+        allowNull: false
+    },
+    id_pregunta: {
+        type: sequelize_1.DataTypes.UUID,
+        allowNull: false
+    },
+    id_opcion: {
+        type: sequelize_1.DataTypes.UUID,
+        allowNull: false
+    },
+    valor_texto: {
+        type: sequelize_1.DataTypes.STRING(255),
+        allowNull: false
+    },
+    valor_numero: {
+        type: sequelize_1.DataTypes.STRING(255),
+        allowNull: false
+    },
+}, {
+    sequelize: connection_1.default,
+    tableName: 'respuesta',
+    timestamps: false,
+});
+exports.default = respuestas;
