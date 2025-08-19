570156c3e1c5cb1a12f1a4f5bfcdf01f07f9c7da
cescDG <cesar199403@hotmail.com>
2025-08-19 12:48:24 -0600

    rep

---
+++ src/app/service/reporte.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Injectable, signal, inject, computed } from '@angular/core';
+import { enviroment } from '../../enviroments/enviroment';
+@Injectable({
+  providedIn: 'root',
+})
+export class CuestionarioService {
+  private myAppUrl: string;
+  private myAPIUrl: string;
+  private myAPIUrl2: string;
+  private http = inject(HttpClient);
+  constructor() {
+    this.myAppUrl = enviroment.endpoint;
+    this.myAPIUrl = 'api/preguntas';
+    this.myAPIUrl2 = 'api/combos';
+  }
+
+  getDependencias(): Observable<any> {
+    return this.http.get<any>(
+      `${this.myAppUrl}${this.myAPIUrl}/getdependencias`
+    );
+  }
+}
