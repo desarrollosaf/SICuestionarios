1a2e7b13c3a58d736fad2cd0840743080fcc372b
MarSGlez <martin.sanchezg@hotmail.com>
2025-08-22 12:39:39 -0600

    cambios connection backend

--- backend/src/database/connection.ts
+++ backend/src/database/connection.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize"
 
-const sequelize = new Sequelize('adminplem_saf', 'root', '', {
-    host: 'localhost',
+const sequelize = new Sequelize('adminplem_saf', 'usr_genero', 'XenCoItYEywGdD1', {
+    host: '192.168.36.53',
     dialect: 'mysql',
     define: {
         freezeTableName: true 
